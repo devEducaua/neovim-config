20fb2dd58ffbe29735516acfa62cedd0bed6772c
devEducaua <schokito4@gmail.com>
2025-07-27 19:22:21 -0300

    style(keymaps): reorganize keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,13 +1,14 @@
-local key = vim.api.nvim_set_keymap
 
 vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
+vim.keymap.set({'n', 'v', 'x', 'c', 't'}, '<C-y>', '"+y', {})
 
-key('n', '<A-f>', "<cmd>Telescope find_files<cr>", { noremap = true, silent = true})
-key('n', '<A-r>', "<cmd>Telescope oldfiles<cr>", { noremap = true, silent = true})
-key('n', '<A-t>', "<cmd>Telescope colorscheme<cr>", { noremap = true, silent = true})
-key('n', '<A-h>', "<cmd>Telescope help_tags<cr>", { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<A-f>', "<cmd>Telescope find_files<cr>", { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<A-r>', "<cmd>Telescope oldfiles<cr>", { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<A-t>', "<cmd>Telescope colorscheme<cr>", { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<A-h>', "<cmd>Telescope help_tags<cr>", { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<A-i>', "<cmd>Telescope highlights<cr>", { noremap = true, silent = true})
 
-key("n", "-", "<cmd>Oil<CR>", {})
-key("n", "<A-p>", ":TransparentToggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "-", "<cmd>Oil<CR>", {})
+vim.api.nvim_set_keymap("n", "<A-p>", ":TransparentToggle<CR>", { noremap = true, silent = true })
 
-key('n', '<space>t', '<cmd>Cmd<CR>', {})
+vim.api.nvim_set_keymap('n', '<space>t', '<cmd>Cmd<CR>', {})
