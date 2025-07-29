8f2e0f288e3a79211f0db50660238971a6cfecde
devEducaua <schokito4@gmail.com>
2025-07-28 23:23:01 -0300

    feat(oil): add scripts to git add and run executable with oil

--- lua/plugins/config/oil.lua
+++ lua/plugins/config/oil.lua
@@ -12,6 +12,22 @@ return function ()
         },
         keymaps = {
             ["g."] = { "actions.toggle_hidden", mode = "n" },
+            [","] = { "actions.cd", mode = "n" },
+            ["gr"] = {
+                desc = "run executable",
+                callback = function ()
+                    local file = require("oil").get_cursor_entry().name
+                    vim.cmd(":Cmd ./" .. file)
+                    vim.cmd("startinsert")
+                end
+            },
+            ["gG"] = {
+                desc = "stage on git",
+                callback = function ()
+                    local file = require("oil").get_cursor_entry().name
+                    vim.cmd(":Cmd git add " .. file)
+                end
+            }
         },
         view_options = {
             show_hidden = true,
