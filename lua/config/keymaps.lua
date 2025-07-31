17e7f03e7e244bcd21c53866eca1e8e5960598bd
devEducaua <schokito4@gmail.com>
2025-07-31 20:35:24 -0300

    feat(keymaps): add keymap for git clone, grep -rn and zoom

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,6 +9,8 @@ vim.api.nvim_create_autocmd("CmdwinEnter", {
   end,
 })
 
+vim.api.nvim_set_keymap('n', '<space>f', ":Zoom<CR>", { noremap = true })
+
 vim.api.nvim_set_keymap('n', '<A-f>', "<cmd>Telescope find_files<cr>", { noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<A-r>', "<cmd>Telescope oldfiles<cr>", { noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<A-t>', "<cmd>Telescope colorscheme<cr>", { noremap = true, silent = true})
@@ -19,6 +21,7 @@ vim.api.nvim_set_keymap("n", "-", "<cmd>Oil<CR>", {})
 vim.api.nvim_set_keymap("n", "<A-p>", ":TransparentToggle<CR>", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('n', '<space>c', ':Cmd ', {})
+vim.api.nvim_set_keymap('n', '<space>s', ';iCmd grep -rn "', {})
 
 vim.api.nvim_set_keymap('n', '<space>g', ':Cmd git ', {})
 vim.api.nvim_set_keymap('n', '<space>gt', ':Cmd git status<CR>', {})
@@ -26,6 +29,7 @@ vim.api.nvim_set_keymap('n', '<space>gf', ':Cmd git diff<CR>', {})
 
 vim.api.nvim_set_keymap('n', '<space>gc', ';iCmd git commit -m "', {})
 vim.api.nvim_set_keymap('n', '<space>gp', ';iCmd git push -u origin main', {})
+vim.api.nvim_set_keymap('n', '<space>gr', ';iCmd git clone ', {})
 
 vim.api.nvim_set_keymap('n', '<space>w', ':w<CR>', {})
 vim.api.nvim_set_keymap('n', '<space>x', ':x<CR>', {})
