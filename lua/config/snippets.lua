3fcf0b53e5074f290ae11e468c83635048f9ac13
devEducaua <schokito4@gmail.com>
2025-08-05 23:29:43 -0300

    feat(snippets): add C snippet for hello

--- lua/config/snippets.lua
+++ lua/config/snippets.lua
@@ -11,8 +11,18 @@ ls.add_snippets("typescript", {
         t('class '), i(1, "name"), t(' {'),
         t({ "", "\t" }), i(0),
         t({ "", "}" }),
-        t({ "", "\t"}), i(0),
+        t({ "", ""}), i(0),
         t({ "", 'export default '}), rep(1), t(";")
     })
 })
 
+ls.add_snippets("c", {
+    s("hello", {
+        t("#include <stdio.h>"),
+        t({ "", "", "" }),
+        t("int main() {"),
+        t({ "", '\tprintf("Hello World\\n");'}),
+        t({ "", '\treturn 0;'}),
+        t({ "", "}" }),
+    })
+})
