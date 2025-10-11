090b9d33caa935e3d19ed8ed7948fdaa464f1964
devEducaua <schokito4@gmail.com>
2025-10-11 12:09:21 -0300

    style(keymaps): padronize quotes

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,35 +3,35 @@ local commands = require("commands")
 local map = vim.keymap.set
 vim.g.mapleader = " "
 
-map({'i', 'v', 't'}, "jk", "<esc>")
+map({"i", "v", "t"}, "jk", "<esc>")
 
 map("t", "<esc>", "<c-\\><c-n>")
-map('n', '<space>;', 'q:', {})
-map('t', '<A-q>', '<esc><esc>bd<CR>')
+map("n", "<space>;", "q:", {})
+map("t", "<A-q>", "<esc><esc>bd<CR>")
 
-map({'n', 'v', 'x', 'c', 't'}, '<C-y>', '"+y', {})
-map({'n', 'v', 'x', 'c', 't'}, '<C-p>', '"+p', {})
-map('i', '<C-p>', '<esc>"+pa')
-map('n', '<C-d>', '<C-d>zz')
-map('n', '<C-u>', '<C-u>zz')
-map('n', '<C-f>', '<C-f>zz')
-map('n', '<C-b>', '<C-b>zz')
+map({"n", "v", "x", "c", "t"}, "<C-y>", '"+y', {})
+map({"n", "v", "x", "c", "t"}, "<C-p>", '"+p', {})
+map("i", "<C-p>", '<esc>"+pa')
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "<C-f>", "<C-f>zz")
+map("n", "<C-b>", "<C-b>zz")
 
-map('n', "<space>c", commands.command)
-map('n', "<space>m", commands.man)
-map('n', '<space>p', commands.permissions)
+map("n", "<space>c", commands.command)
+map("n", "<space>m", commands.man)
+map("n", "<space>p", commands.permissions)
 
-map('n', '<leader>gg', ':Cmd git ', {})
-map('n', '<leader>gt', ':Cmd git status<CR>', {})
-map('n', '<leader>gd', '<space>;iCmd git diff ', {})
-map('n', '<leader>gco', '<space>;iCmd git commit -m "', {})
-map('n', '<leader>gp', '<space>;iCmd git push -u origin main', {})
-map('n', '<leader>gcr', '<space>;iCmd git clone ', {})
+map("n", "<leader>gg", ":Cmd git ", {})
+map("n", "<leader>gt", ":Cmd git status<CR>", {})
+map("n", "<leader>gd", "<space>;iCmd git diff ", {})
+map("n", "<leader>gco", '<space>;iCmd git commit -m "', {})
+map("n", "<leader>gp", "<space>;iCmd git push -u origin main", {})
+map("n", "<leader>gcr", "<space>;iCmd git clone ", {})
 
-map('n', '<leader>w', ':up<CR>', {})
-map('n', '<leader>x', ':up<CR> :Oil<CR>', {})
-map('n', '<leader>!', ':q!<CR>', {})
-map('n', '<leader>q', ':bd!<CR>', {})
+map("n", "<leader>w", ":up<CR>", {})
+map("n", "<leader>x", ":up<CR> :Oil<CR>", {})
+map("n", "<leader>!", ":q!<CR>", {})
+map("n", "<leader>q", ":bd!<CR>", {})
 map("n", "<leader>s", ":w<CR>:so<CR>", {})
 
 map("n", "<leader>f", ":Telescope find_files<CR>")
@@ -40,13 +40,13 @@ map("n", "<leader>th", ":Telescope help_tags<CR>")
 
 map("n", "-", "<cmd>Oil<CR>", {})
 
-map({'i', 'n', 'v', 'x', 'c', 't'}, '<A-v>', function ()
+map({"i", "n", "v", "x", "c", "t"}, "<A-v>", function ()
     vim.cmd(":vsplit")
     vim.cmd.wincmd("L")
     vim.cmd("Telescope find_files")
 end)
 
-map({'i', 'n', 'v', 'x', 'c', 't'}, '<A-V>', function ()
+map({"i", "n", "v", "x", "c", "t"}, "<A-V>", function ()
     vim.cmd(":split")
     vim.cmd.wincmd("J")
     vim.cmd("Telescope find_files")
@@ -58,7 +58,7 @@ vim.api.nvim_create_autocmd("CmdwinEnter", {
   end,
 })
 
-vim.api.nvim_create_autocmd('BufEnter', {
+vim.api.nvim_create_autocmd("BufEnter", {
     pattern = { "c", "sh", "man" },
     callback = function ()
         map("n", "K", function ()
@@ -67,9 +67,9 @@ vim.api.nvim_create_autocmd('BufEnter', {
     end
 })
 
-vim.api.nvim_create_autocmd('FileType', {
+vim.api.nvim_create_autocmd("FileType", {
     pattern = "man",
     callback = function ()
-        map('n', '<A-l>', function() vim.opt_local.relativenumber = not vim.opt_local.relativenumber:get() end, { buffer = true })
+        map("n", "<A-l>", function() vim.opt_local.relativenumber = not vim.opt_local.relativenumber:get() end, { buffer = true })
     end
 })
