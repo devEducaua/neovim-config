7e47133c7f39f196300d8f7d601ca66607791c92
devEducaua <schokito4@gmail.com>
2025-10-11 11:46:45 -0300

    fix(telescope): remove unnecessary options

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,8 +1,4 @@
 return function ()
-    -- vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none", fg = "none" })
-    -- vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none", fg = "none" })
-    -- vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none", fg = "none" })
-
     require("telescope").setup({
         file_ignore_patterns = { "^node_modules/" },
         defaults = {
@@ -16,15 +12,6 @@ return function ()
                 height = 0.15,
                 preview_width = 0
             },
-
-            -- mappings = {
-            --     i = {
-            --         vim.api.nvim_set_keymap('n', '<A-f>', "<cmd>Telescope find_files<cr>", { noremap = true, silent = true}),
-            --         vim.api.nvim_set_keymap('n', '<A-r>', "<cmd>Telescope oldfiles<cr>", { noremap = true, silent = true}),
-            --         vim.api.nvim_set_keymap('n', '<A-t>', "<cmd>Telescope colorscheme<cr>", { noremap = true, silent = true}),
-            --         vim.api.nvim_set_keymap('n', '<A-h>', "<cmd>Telescope help_tags<cr>", { noremap = true, silent = true})
-            --     }
-            -- }
         }
     })
 end
